7c01f8ac7972d5987c8b0033b7f9cd75e90b6d65
fadeevao <olgafadejeva11@gmail.com>
2020-07-01 21:35:12 +0100

    Started work on the gallery

---
+++ src/components/Gallery/Gallery.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import ImageGallery from "react-image-gallery";
+import styled from "styled-components";
+
+const GalleryContainer = styled.div`
+  padding: 5rem 20rem;
+  @media screen and (max-width: 1400px) {
+    padding: 0 0;
+  }
+`;
+
+export default function Gallery() {
+  const images = [
+    {
+      original: "https://picsum.photos/id/1018/1000/600/",
+      thumbnail: "https://picsum.photos/id/1018/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1015/1000/600/",
+      thumbnail: "https://picsum.photos/id/1015/250/150/",
+    },
+    {
+      original: "https://picsum.photos/id/1019/1000/600/",
+      thumbnail: "https://picsum.photos/id/1019/250/150/",
+    },
+  ];
+
+  return (
+    <GalleryContainer>
+      <ImageGallery items={images} showPlayButton={false} />
+    </GalleryContainer>
+  );
+}
--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Section from "../Section";
 import PageHeader from "../PageHeader";
+import Gallery from "../Gallery";
 import styled from "styled-components";
 
 var Container = styled.div`
@@ -59,15 +60,11 @@ export default function Layout() {
             nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat
             volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation
             ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo
-            consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate
-            velit esse molestie consequat, vel illum dolore eu feugiat nulla
-            facilisis at vero eros et accumsan et iusto odio dignissim qui
-            blandit praesent luptatum zzril delenit augue duis dolore te feugait
-            nulla facilisi. Nam liber tempor cum soluta nobis eleifend option
-            congue nihil imperdiet doming id quod mazim placerat facer possim
-            assum.
           </AboutMeContent>
         </SectionContent>
+      </Section>
+      <Section>
+        <Gallery />
       </Section>
 
       <Section id="gallery" />
